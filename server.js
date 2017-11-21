28b612ac9c63bea106aa782dbfbd0f92e2a2db4d
h4tl2 <hat9458g@gmail.com>
2017-11-21 15:06:53 +0700

    fix con

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var mongoose = require('./config/mongoose');
 var express = require('./config/express');
-var db = mongoose();
+var db = mongoose.connection;
 var app = express();
+
+
 app.listen(8082);
 
 console.log('server running');
